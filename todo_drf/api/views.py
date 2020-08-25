4c5a6d39c066f932155377d758f38d585bd96b1d
Shiv1202 <shivansht9211@gmail.com>
2020-08-25 12:18:38 +0530

    first commit

---
+++ todo_drf/api/views.py
@@ -0,0 +1,67 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
+
+# Create your views here.
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List' : '/task-list/',
+        'Detail View' : '/task-detail/<str:pk>/',
+        'Create' : '/task-create/',
+        'Update' : '/task-update/<str:pk>/',
+        'Delete' : '/task-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+"""
+Below Function going to display all the tasks store in the data base.
+"""
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many = True)
+    return Response(serializer.data)
+
+"""
+This Function going to display Detailed view of one perticuler task with the help of pk.
+"""
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many = False)
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = TaskSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+
+@api_view(['POST'])
+def taskUpdate(request, pk):
+    task = Task.objects.get(id = pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def taskDelete(request, pk):
+    task = Task.objects.get(id = pk)
+    task.delete()
+    return Response("Taks deleted successfully.")
+
+
